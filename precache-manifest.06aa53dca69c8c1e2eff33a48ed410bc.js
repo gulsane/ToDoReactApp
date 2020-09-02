5bc2d63f5932271b29603492e096e8f6429ca86d
foo <foo@gmail.com>
2020-09-03 02:32:19 +0530

    Updates

---
+++ precache-manifest.06aa53dca69c8c1e2eff33a48ed410bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b473d15e716a101b5c803fee848d5661",
+    "url": "/.github.io/ToDoReactApp/index.html"
+  },
+  {
+    "revision": "593dc32e368f615a127f",
+    "url": "/.github.io/ToDoReactApp/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d0b00ef6ca653b8bf878",
+    "url": "/.github.io/ToDoReactApp/static/js/2.e7a27fc6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/.github.io/ToDoReactApp/static/js/2.e7a27fc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "593dc32e368f615a127f",
+    "url": "/.github.io/ToDoReactApp/static/js/main.9133b4af.chunk.js"
+  },
+  {
+    "revision": "cfe35d9d4c6aefb7bfb5",
+    "url": "/.github.io/ToDoReactApp/static/js/runtime-main.4991e628.js"
+  }
+]);
